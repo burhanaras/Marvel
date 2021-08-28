9bd0a4ae915d51da99bb3aad1b09a30adedbc4fd
Burhan Aras <burhan.aras@paydustry.com>
2021-08-28 15:52:28 +0300

    Developed UI list with dummy data

--- Modern-SwiftUI-MVVM-Tests/UI/Product List/ProductListView.swift
+++ Modern-SwiftUI-MVVM-Tests/UI/Product List/ProductListView.swift
@@ -8,13 +8,53 @@
 import SwiftUI
 
 struct ProductListView: View {
+    @ObservedObject var viewModel: ProductListViewModel
+    
+    init(viewModel: ProductListViewModel) {
+        _viewModel = ObservedObject(wrappedValue: viewModel)
+    }
     var body: some View {
-        Text("Hello, World!")
+        NavigationView{
+            switch viewModel.data{
+            case let .success(products): ProductList(products: products)
+            case let .failure(error):
+                Text("Fail")
+            case .none:
+                ProgressView()
+            }
+        }
+        .navigationTitle("Products")
+        .navigationViewStyle(StackNavigationViewStyle())
+    }
+}
+
+struct ProductList: View{
+    let products: [Product]
+    var body: some View{
+        List(products){ product in
+            ProductView(product: product)
+        }
+        .navigationTitle("Products")
+    }
+}
+
+struct ProductView: View{
+    let product: Product
+    var body: some View{
+        NavigationLink(
+            destination: Text("Destination"),
+            label: {
+                HStack{
+                    Text(product.title)
+                    Spacer()
+                    Text(product.price)
+                }
+            })
     }
 }
 
 struct ProductListView_Previews: PreviewProvider {
     static var previews: some View {
-        ProductListView()
+        ProductListView(viewModel: ProductListViewModel())
     }
 }
--- Modern-SwiftUI-MVVM-Tests/UI/Product List/ProductListViewModel.swift
+++ Modern-SwiftUI-MVVM-Tests/UI/Product List/ProductListViewModel.swift
@@ -8,5 +8,25 @@
 import Foundation
 
 class ProductListViewModel: ObservableObject{
+    @Published private(set) var data: Result<[Product], CommonError>? = .none
     
+    init() {
+        subscribe()
+    }
 }
+
+extension ProductListViewModel {
+    private func subscribe() {
+        data = .success(dummyProducts)
+    }
+}
+
+
+// Dummy data
+
+let dummyProduct0 = Product(id: "0", title: "Peoduct 0", price: "15.00 USD")
+let dummyProduct1 = Product(id: "1", title: "Peoduct 1", price: "15.00 USD")
+let dummyProduct2 = Product(id: "2", title: "Peoduct 2", price: "15.00 USD")
+let dummyProduct3 = Product(id: "3", title: "Peoduct 3", price: "15.00 USD")
+let dummyProduct4 = Product(id: "4", title: "Peoduct 4", price: "15.00 USD")
+let dummyProducts = [dummyProduct0, dummyProduct1, dummyProduct2, dummyProduct3, dummyProduct4]
