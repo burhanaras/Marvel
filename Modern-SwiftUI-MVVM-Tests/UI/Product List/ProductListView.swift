0e98c2695ba5beb4d9bcc548f4ea5b50dc8493a0
Burhan Aras <burhan.aras@paydustry.com>
2021-08-28 15:17:33 +0300

    Created the first versions of product list.

---
+++ Modern-SwiftUI-MVVM-Tests/UI/Product List/ProductListView.swift
@@ -0,0 +1,20 @@
+//
+//  ProductListView.swift
+//  Modern-SwiftUI-MVVM-Tests
+//
+//  Created by Burhan Aras on 28.08.2021.
+//
+
+import SwiftUI
+
+struct ProductListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProductListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductListView()
+    }
+}
---
+++ Modern-SwiftUI-MVVM-Tests/UI/Product List/ProductListViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ProductListViewModel.swift
+//  Modern-SwiftUI-MVVM-Tests
+//
+//  Created by Burhan Aras on 28.08.2021.
+//
+
+import Foundation
+
+class ProductListViewModel: ObservableObject{
+    
+}
