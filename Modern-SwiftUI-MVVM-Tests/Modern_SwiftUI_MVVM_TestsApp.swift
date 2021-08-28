64441918f6ebeca3a69945580fbefc5660b5199f
Burhan Aras <burhan.aras@paydustry.com>
2021-08-28 14:57:25 +0300

    Initial Commit

---
+++ Modern-SwiftUI-MVVM-Tests/Modern_SwiftUI_MVVM_TestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Modern_SwiftUI_MVVM_TestsApp.swift
+//  Modern-SwiftUI-MVVM-Tests
+//
+//  Created by Burhan Aras on 28.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct Modern_SwiftUI_MVVM_TestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
