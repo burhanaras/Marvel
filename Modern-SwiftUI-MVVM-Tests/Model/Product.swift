f61f415053321d8e92678c2f451eb48811fda9b3
Burhan Aras <burhan.aras@paydustry.com>
2021-08-28 15:24:45 +0300

    Created model objects for product.

---
+++ Modern-SwiftUI-MVVM-Tests/Model/Product.swift
@@ -0,0 +1,14 @@
+//
+//  Product.swift
+//  Modern-SwiftUI-MVVM-Tests
+//
+//  Created by Burhan Aras on 28.08.2021.
+//
+
+import Foundation
+
+struct Product: Identifiable{
+    let id: String
+    let title: String
+    let price: String
+}
